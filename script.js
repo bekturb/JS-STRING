db4e7c26370bf03ee59d9757602244fc798ed428
bekturb <bekkgboy2@gmail.com>
2022-03-01 05:39:12 +0600

    initial

---
+++ script.js
@@ -0,0 +1,79 @@
+    // String
+
+// let user = 'Bektursun';
+// let userName = "Bektursun";
+// let words = "He said: I 'm a doctor";
+// let word = 'He said: "I "m a doctor"';
+// let userWord = 'He said: "I \'m a doctor"';
+//     let myString = `He said: "I 'm a doctor"`
+// let myString2 = `He said: "I 'm  ${user}"`
+
+//
+//     console.log(user);
+//     console.log( userName);
+//     console.log( words);
+//     console.log( word );
+//     console.log(userWord);
+//     console.log(myString)
+//     console.log(myString2)
+
+
+    // let myString = 'Bektursun';
+    // console.log(myString.length) // 9
+    // console.log(myString[0]) // B
+
+// let userName = prompt('What is your name?');
+
+// let userAnswer = userName.toUpperCase(); // BEKTURSUN
+// let userAnswer = userName.toLowerCase(); // bektursun
+// document.write(userAnswer);
+
+// let name = 'bektursun How old are you?';
+//     // console.log(name.trim());
+//     // console.log(name.concat("Hello again"));
+//     // console.log(name.trim().repeat(3));
+//     // console.log(name.includes('old are bektursun')) // false
+//     // console.log(name.indexOf('t')) //3
+//     // console.log(name.lastIndexOf('u')) //24
+//     // console.log(name.charAt(3)) //t
+//     // console.log(name.substring(0,3)) // bek
+//     // console.log(name.replace('bektursun', 'baktiyar')) //baktiyar How old are you?
+//     // console.log(name.slice(1,6)) // ektur
+//     // console.log(name.substr(0, 3)) //bek
+//     // console.log(name.search('How'))// 10
+
+            //exercise
+
+    // let userName = prompt('What is your name?');
+    // document.write(`"Hello, ${userName}!"`);
+
+    // let userName = prompt('What is your favorite football team?');
+    // document.write(`${userName.toUpperCase()} is  champion!"`);
+
+
+    // let userName = prompt("What is your credit card's number");
+    // document.write(userName.slice(-5,-1));
+
+                        //HOMETASK
+//     let userName = 'iVan';
+// let name = userName[0].toUpperCase() + userName.slice(1).toLowerCase()
+//     console.log(name);
+//
+//     let userName = 'Bektursun';
+//     console.log(userName.slice(1))
+
+    // let amount = '"$120"'
+    // console.log(amount.replace('$',''))
+
+    // let computer = prompt('Здравствуй, я компьютер, а тебя как зовут?');
+    // let computer2 = prompt(`Очень приятно, ${computer} . Сколько тебе лет?`);
+    // let computer3 = prompt(`Ого! Тебе уже ${computer2} !`);
+
+// let cardNumber = prompt('Type your credit card number!')
+//     document.write(cardNumber.replace(cardNumber.slice(0, -4), '*') + cardNumber.slice(-4))
+    // document.write(cardNumber.replace(/[0-9]/g,'*') + cardNumber.slice(-4));
+
+
+// let userName = 'Bektursun';
+// let name = userName.slice(0, -4)
+//     console.log(name.replace("bektu",''))
